63875cb7fd268243d8b744deb9fa08c54bf5e574
Ricardo Manuel Ribeiro Alcobia <ricmanue@c1r11s2.42lisboa.com>
2024-12-02 11:48:48 +0000

    first commit, structur

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 11:31:39 by ricmanue          #+#    #+#             */
+/*   Updated: 2024/12/02 11:42:09 by ricmanue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include "libft.h"
+#include "ft_printf.h"
+
+#endif
