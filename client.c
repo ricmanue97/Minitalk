6be3af0182f03a2d421b66751f39f4d29541cb9f
Ricardo Manuel Ribeiro Alcobia <ricmanue@c1r11s2.42lisboa.com>
2024-12-03 13:49:01 +0000

    client

--- client.c
+++ client.c
@@ -6,11 +6,53 @@
 /*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 11:32:20 by ricmanue          #+#    #+#             */
-/*   Updated: 2024/12/02 12:04:45 by ricmanue         ###   ########.fr       */
+/*   Updated: 2024/12/03 13:48:20 by ricmanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static int	g_signal;
+
+void signal_handler(int sig_nbr)
+{
+	static int	bits;
+
+	g_signal = true;
+	if (sig_nbr == SIGUSR1)
+		bits++;
+	else if (sig_nbr == SIGUSR2)
+		ft_printf("This message was ->%d bytes", bits / 8);
+}
+
+int	ft_ascii_to_bin(char letter, int pid)
+{
+	int	i;
+	int	bin;
+
+	bin = 7;
+	while (bin >= 0)
+	{
+		i = 0;
+		if((letter >> bin) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (g_signal == false)
+		{
+			if (i == 10)
+			{
+				ft_putstr_fd("Server not responding\n", 2);
+				exit(0);
+			}
+			i++;
+			usleep(1000000);
+		}
+		g_signal = false;
+		bin--;
+	}
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
--- minitalk.h
+++ minitalk.h
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ricmanue < ricmanue@student.42lisboa.co    +#+  +:+       +#+        */
+/*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 11:31:39 by ricmanue          #+#    #+#             */
-/*   Updated: 2024/12/02 13:51:16 by ricmanue         ###   ########.fr       */
+/*   Updated: 2024/12/03 13:00:13 by ricmanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+# define _DEFAULT_SOURCE
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <stdbool.h>
 #include "libft.h"
 #include "ft_printf.h"
 
